e416e0cf38113da2190cfc58f8fb963b5be61dbe
rusk <lordruskk@gmail.com>
2020-07-22 18:19:36 -0700

    proper error message

--- goscrape.go
+++ goscrape.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"os"
-	"io"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"regexp"
 	"strings"
-	"net/http"
 
 	"github.com/monaco-io/request"
 )
@@ -15,12 +15,16 @@ func download(imageurls []string, urlnum int, c chan int) {
 	pimageurl := strings.Split(imageurls[urlnum], "/") /* looks like [ i.4cdn.org g 244211] */
 	filename := pimageurl[2]
 
-	response, err := http.Get("https://"+imageurls[urlnum])
-	if err != nil { fmt.Println("Error downloading", filename) }
+	response, err := http.Get("https://" + imageurls[urlnum])
+	if err != nil {
+		fmt.Println("Error downloading", filename)
+	}
 	defer response.Body.Close()
 
 	file, err := os.Create(filename)
-	if err != nil { fmt.Println("Error downloading", filename) }
+	if err != nil {
+		fmt.Println("Error downloading", filename)
+	}
 	defer file.Close()
 
 	io.Copy(file, response.Body)
@@ -47,13 +51,13 @@ func main() {
 		url := urls[i]
 
 		/* grab page info */
-		client := request.Client {
-			URL: url,
+		client := request.Client{
+			URL:    url,
 			Method: "GET",
 		}
 		resp, err := client.Do()
 		if err != nil {
-			fmt.Println("Invalid URL!")
+			fmt.Println("Unable to reach", url, "\nCheck your internet and make sure the URL is correct")
 			os.Exit(0)
 		}
 
@@ -71,19 +75,21 @@ func main() {
 				imagemap[item] = true
 			}
 		}
-      		var imageurls []string
-      		for item, _ := range imagemap { imageurls = append(imageurls, item) }
+		var imageurls []string
+		for item, _ := range imagemap {
+			imageurls = append(imageurls, item)
+		}
 
 		/* directory stuff */
 		if len(os.Args) > 2 {
-			if err := os.Chdir(string(os.Args[2])+"/"); err != nil {
+			if err := os.Chdir(string(os.Args[2]) + "/"); err != nil {
 				fmt.Println("Error! Directory does not exist!")
 				os.Exit(0)
 			}
 		} else {
 			purl := strings.Split(url, "/") /* url looks like [https:  4chan.org g 4532123] */
 			os.MkdirAll(purl[3]+"/"+purl[5], os.ModePerm)
-			os.Chdir(purl[3]+"/"+purl[5])
+			os.Chdir(purl[3] + "/" + purl[5])
 		}
 
 		fmt.Println("Downloading", url)
@@ -94,7 +100,7 @@ func main() {
 		}
 
 		for i := 0; i < len(imageurls); i++ {
-			c := <- dlc
+			c := <-dlc
 			pimageurl := strings.Split(imageurls[c], "/") /* looks like [ i.4cdn.org g 244211] */
 			fmt.Println("Finished downloading", pimageurl[2])
 		}
