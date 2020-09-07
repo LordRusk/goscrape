2b8d22cb811aedd173695df7e65e1539ec7d3236
lordrusk <lordruskk@gmail.com>
2020-09-07 14:31:20 +0000

    new api

--- goscrape.go
+++ goscrape.go
@@ -4,22 +4,22 @@ import (
 	"os"
 	"io"
 	"log"
-	"regexp"
 	"errors"
 	"strings"
+	"strconv"
 	"net/http"
 
-	"github.com/monaco-io/request"
+	"github.com/mtarnawa/godesu"
 )
 
 type finishState struct {
-	filename string
+	image godesu.Image
 	err error
 }
 
-func download(imageUrls []string, urlNum int, finishStateChan chan finishState) {
-	pimageUrl := strings.Split(imageUrls[urlNum], "/") /* looks like [ i.4cdn.org g 244211.jpg] */
-	filename := pimageUrl[2]
+func download(image godesu.Image, finishStateChan chan finishState) {
+	var filename string
+	filename = (image.Filename+image.Extension)
 
 	if _, err := os.Stat(filename); err == nil {
 		var err strings.Builder
@@ -27,7 +27,7 @@ func download(imageUrls []string, urlNum int, finishStateChan chan finishState) 
 		err.WriteString(" exists! Skipping...")
 
 		fs := finishState {
-			filename: filename,
+			image: image,
 			err: errors.New(err.String()),
 		}
 
@@ -35,14 +35,14 @@ func download(imageUrls []string, urlNum int, finishStateChan chan finishState) 
 		return
 	}
 
-	response, err := http.Get("https://"+imageUrls[urlNum])
+	response, err := http.Get(image.URL)
 	if err != nil {
 		var err strings.Builder
 		err.WriteString("Error downloading ")
 		err.WriteString(filename)
 
 		fs := finishState {
-			filename: filename,
+			image: image,
 			err: errors.New(err.String()),
 		}
 
@@ -52,7 +52,7 @@ func download(imageUrls []string, urlNum int, finishStateChan chan finishState) 
 
 	/* Use a temp file name to avoid half downloaded
 	 * images if goscrape were to be killed then restarted
-	 * on the same thread(s) caused by goscrape skipping
+	 * on the same Thread(s) caused by goscrape skipping
 	 * images that already exist.
 	 */
 	var tmpFilename strings.Builder
@@ -66,7 +66,7 @@ func download(imageUrls []string, urlNum int, finishStateChan chan finishState) 
 		err.WriteString(filename)
 
 		fs := finishState {
-			filename: filename,
+			image: image,
 			err: errors.New(err.String()),
 		}
 		finishStateChan <- fs
@@ -80,7 +80,7 @@ func download(imageUrls []string, urlNum int, finishStateChan chan finishState) 
 	}
 
 	fs := finishState {
-		filename: filename,
+		image: image,
 		err: nil,
 	}
 
@@ -91,62 +91,41 @@ func download(imageUrls []string, urlNum int, finishStateChan chan finishState) 
 func main() {
 	if len(os.Args) < 2 {
 		log.Printf("Error! No url(s) specified!\n\nFirst arg: Url(s) *must be in quotes if multiple urls*\nSecond arg: Custom download directory\n")
-		os.Exit(0)
+		os.Exit(1)
 	}
 
 	urls := strings.Split(os.Args[1], " ")
-
 	origDir, _ := os.Getwd()
-
 	dlc := make(chan finishState)
+	Gochan := godesu.New()
 
 	/* loop through all urls */
 	for urlNum, url := range urls {
 		os.Chdir(origDir)
 
-		/* grab page info */
-		client := request.Client {
-			URL: url,
-			Method: "GET",
-		}
-		resp, err := client.Do()
+		/* godesu suff */
+		purl := strings.Split(url, "/")
+		ThreadNum, _ := strconv.Atoi(purl[5])
+		err, Thread := Gochan.Board(purl[3]).GetThread(ThreadNum)
 		if err != nil {
-			log.Println("Unable to reach", url + "!\nCheck your connection and make sure the url is correct.")
-			os.Exit(0)
+			log.Fatal(errors.New("Error! Could not fetch Thread!"))
 		}
-
-		/* get links to the images */
-		lpat1 := regexp.MustCompile(`i\.4cdn\.org/[a-z]+/[0-9]*\.(png|jpg|gif|webm)`)
-		lpat2 := regexp.MustCompile(`is2\.4chan\.org/[a-z]+/[0-9]*\.(png|jpg|gif|webm)`)
-		imageUrls1 := lpat1.FindAllString(string(resp.Data), -1)
-		imageUrls2 := lpat2.FindAllString(string(resp.Data), -1)
-		imageUrls1 = append(imageUrls1, imageUrls2...)
-
-		/* remove duplicate links caused by thumbnails */
-		imageMap := make(map[string]bool)
-		for _, item := range imageUrls1 {
-			if _, ok := imageMap[item]; !ok {
-				imageMap[item] = true
-			}
-		}
-      		var imageUrls []string
-      		for item, _ := range imageMap { imageUrls = append(imageUrls, item) }
+		images := Thread.Images()
 
 		/* directory stuff */
 		if len(os.Args) > 2 {
 			if err := os.Chdir(string(os.Args[2])+"/"); err != nil {
 				if err := os.MkdirAll(string(os.Args[2]+"/"), os.ModePerm); err != nil {
 					log.Println("Error! Cannot create custom directory! Check permissions!")
-					os.Exit(0)
+					os.Exit(1)
 				} else {
 					os.Chdir(string(os.Args[2])+"/")
 				}
 			}
 		} else {
-			purl := strings.Split(url, "/") /* url looks like [https:  4chan.org g 4532123] */
 			if err := os.MkdirAll(purl[3]+"/"+purl[5], os.ModePerm); err != nil {
 				log.Println("Error! Cannot create directory! Check permissions")
-				os.Exit(0)
+				os.Exit(1)
 			}
 			os.Chdir(purl[3]+"/"+purl[5])
 		}
@@ -154,16 +133,16 @@ func main() {
 		log.Println("Downloading", url, urlNum+1, "of", len(urls))
 
 		/* download all the images */
-		for urlNum, _ := range imageUrls {
-			go download(imageUrls, urlNum, dlc)
+		for _, image := range images {
+			go download(image, dlc)
 		}
 
-		for i := 0; i < len(imageUrls); i++ {
+		for i := 0; i < len(images); i++ {
 			fs := <-dlc
 			if fs.err != nil {
-				log.Println(fs.err, i+1, "of", len(imageUrls))
+				log.Println(fs.err, i+1, "of", len(images))
 			} else {
-				log.Println("Finished downloading", fs.filename, i+1, "of", len(imageUrls))
+				log.Println("Finished downloading", fs.image.Filename+fs.image.Extension, i+1, "of", len(images))
 			}
 		}
 	}
--- readme.md
+++ readme.md
@@ -9,15 +9,11 @@ run `goscrape` with the first argument being the link to the thread. Put in quot
 
 ## Features
 * Goscrape is upwards of 4x faster then other scrapers, goscrape does this by using go's concurrency to download multiple images at the same time, taking advantage of more bandwidth. You won't find download speeds like this anywhere else.
+* Goscrape uses the [godesu](https://github.com/mtarnawa/godesu) 4chan read-only api to interact with 4chan. The old system used a request library, regex, maps, etc, but this seems to fit more with the go style.
 * Goscrape is cross-platform, works everywhere that go does out of the box! (Plan9, etc, etc)
 
 ## Why?
 Because *all* 4chan scrapers I've seen and used were written in python, I dislike python and wanted to make something in GO.
-
-## FQA - Frequently Questioned Answers
-+ Why do you use `github.com/monaco-io/request` as the request library and not `net/http`?
-
-Because `/net/http` doesn't give me the information I need from 4chan.
 
 ### License
 Goscrape is in the public domain.
